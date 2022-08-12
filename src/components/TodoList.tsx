58bf382eff205bfb081d477efa8600b1dea1d38c
Romen, Ter Gazarian <romen.tergazarian@gmail.com>
2022-08-12 12:04:55 -0300

    [1]Prueba fix --force para el uso de react-beautiful-dnd

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+import "./styles.css"
+
+
+
+interface Props{
+    todos:Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    completedTodo: Todo[];
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+
+}
+
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+  return (
+    
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+        </span>
+        {
+          todos.map((todo) => (
+            <SingleTodo
+              todo={todo}
+              todos={todos}
+              key={todo.id}
+              setTodos={setTodos}
+            />
+          ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">
+            Complete Tasks
+          </span>
+          {
+            todos.map((todo) => (
+              <SingleTodo
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+      </div>
+    </div>
+  )
+}
+
+export default TodoList
+
+
